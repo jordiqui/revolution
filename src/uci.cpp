78e2cb00762b8ec8a8d0107aed2624929e69227b
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-09-03 14:35:11 +0200

    feat: update version to v1.2 with automatic build date

--- src/uci.cpp
+++ src/uci.cpp
@@ -43,7 +43,7 @@
 #include "ucioption.h"
 
 #ifndef ENGINE_NAME
-    #define ENGINE_NAME "revolution dev 010925 v1.0.1"
+    #define ENGINE_NAME "revolution dev v1.2"
 #endif
 #ifndef ENGINE_BUILD_DATE
     #define ENGINE_BUILD_DATE __DATE__
