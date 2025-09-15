b738377c013f374c7213910b34480b345ff8561b
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-09-16 01:22:22 +0200

    Restore revolution-dev v2.40 version identifiers

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifndef ENGINE_NAME
-    #define ENGINE_NAME "Revolution v.2.41 dev-150925"
+    #define ENGINE_NAME "revolution-dev v.2.40 130925"
 #endif
 
 #ifndef ENGINE_BUILD_DATE
