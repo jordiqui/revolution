93e846effc78d00eae5854460745bbc403435d46
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-09-21 12:50:05 +0200

    Update UCI name comment

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -19,13 +19,8 @@
 */
 
 #include "ucioption.h"
-// --- Engine identity (fallbacks; Makefile can override) ---
-#ifndef ENGINE_NAME
-    #define ENGINE_NAME "Revolution 2.70 210925"
-#endif
-#ifndef ENGINE_BUILD_DATE
-    #define ENGINE_BUILD_DATE ""  // build identifier
-#endif
+
+#include "version.h"
 
 
 #include <algorithm>
