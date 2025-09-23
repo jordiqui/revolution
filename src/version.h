c791f4e519a3e467396209a7bed35cb910b09048
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-09-23 04:13:48 +0200

    Document updated engine name default

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifndef ENGINE_NAME
-    #define ENGINE_NAME "revolution 2.73-dev-230925-wk"
+    #define ENGINE_NAME "revolution-2.73-dev-230925-wk"
 #endif
 
 #ifndef ENGINE_BUILD_DATE
