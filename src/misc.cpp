b60536b4d96b0fe8c6619efec81315b8d4b580b2
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-11-18 10:28:51 +0100

    Revert "Merge pull request #125 from jordiqui/codex/rebrand-application-to-revolution-3.60-181125"
    This reverts commit 3765ef5d23e6b251aa855e7c374333bb0409a757, reversing
    changes made to e75ad83f261d286b6fea37a3bcb872abe8955f36.

--- src/misc.cpp
+++ src/misc.cpp
@@ -42,8 +42,6 @@ constexpr std::string_view kEngineNameShort    = "Revolution-3.60-181125";
 constexpr std::string_view kEngineDisplayName  = "Revolution-3.60-181125 - UCI chess engine";
 constexpr std::string_view kEngineUciHeader    =
     "Revolution-3.60 Developed by Jorge Ruiz and the Stockfish developers (see AUTHORS file)";
-constexpr std::string_view kEngineAnalysisTag =
-    "Revolution-3.60-181125 (update scripts Revolution-3.60-181125)";
 
 // Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 // cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
@@ -118,7 +116,7 @@ class Logger {
 
 // Returns the short public identification string for the Revolution engine.
 std::string engine_version_info() {
-    return std::string(kEngineAnalysisTag);
+    return std::string(kEngineNameShort);
 }
 
 std::string engine_info(bool to_uci) {
