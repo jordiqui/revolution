afca21a5088629b471637882f7c470506f033561
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2026-01-23 21:47:23 +0100

    Update NNUE defaults and net handling

---
+++ src/nnue/evaluate.h
@@ -0,0 +1,10 @@
+#ifndef REVOLUTION_NNUE_EVALUATE_H_INCLUDED
+#define REVOLUTION_NNUE_EVALUATE_H_INCLUDED
+
+// This header is used by src/nnue/net.sh to locate the default NNUE filenames.
+// Keep these in sync with ../evaluate.h.
+
+#define EvalFileDefaultNameBig "nn-c288c895ea92.nnue"
+#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+
+#endif  // REVOLUTION_NNUE_EVALUATE_H_INCLUDED
