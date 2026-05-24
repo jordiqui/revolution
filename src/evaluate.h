d732a59f84f9a6769cf43626d769ac88254db571
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2026-05-24 20:28:41 +0200

    REVOLUTION-P0A: align dual-net big NNUE authority

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-fcf986aea78a.nnue"
+#define EvalFileDefaultNameBig "nn-83a0d6daf7e5.nnue"
 #define EvalFileDefaultNameSmall "nn-47fc8b7fff06.nnue"
 
 namespace NNUE {
--- src/nnue/evaluate.h
+++ src/nnue/evaluate.h
@@ -4,7 +4,7 @@
 // This header is used by src/nnue/net.sh to locate the default NNUE filenames.
 // Keep these in sync with ../evaluate.h.
 
-#define EvalFileDefaultNameBig "nn-fcf986aea78a.nnue"
+#define EvalFileDefaultNameBig "nn-83a0d6daf7e5.nnue"
 #define EvalFileDefaultNameSmall "nn-47fc8b7fff06.nnue"
 
 #endif  // REVOLUTION_NNUE_EVALUATE_H_INCLUDED
