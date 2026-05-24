87cae0139203853e1692de14d292f0890b3708c5
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2026-05-25 01:19:43 +0200

    REVOLUTION-P0V: remove dual small-net export surface

--- src/engine.h
+++ src/engine.h
@@ -94,7 +94,6 @@ class Engine {
     void load_network(const std::string& file);
     void load_big_network(const std::string& file);
     void save_network(const std::pair<std::optional<std::string>, std::string>& file);
-    void save_network(const std::pair<std::optional<std::string>, std::string> files[2]);
 
     // utility functions
 
