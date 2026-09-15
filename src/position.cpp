48f09ca3d717a61b1ec6f89f6393fcdda8676e90
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2026-09-15 13:25:32 +0200

    P0A11-SF20260903: apply Stockfish Sep3 topology block

--- src/position.cpp
+++ src/position.cpp
@@ -1512,7 +1512,7 @@ void Position::flip() {
     f += token + " ";
 
     std::transform(f.begin(), f.end(), f.begin(),
-                   [](char c) { return char(islower(c) ? toupper(c) : tolower(c)); });
+                   [](unsigned char c) { return char(islower(c) ? toupper(c) : tolower(c)); });
 
     ss >> token;  // En passant square
     f += (token == "-" ? token : token.replace(1, 1, token[1] == '3' ? "6" : "3"));
